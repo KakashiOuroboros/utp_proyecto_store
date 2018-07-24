5fa683fb7c8b69d7ddf383facab40c3a649dd789
Isaac Quinzada <marcosiasouroboros@gmail.com>
2018-07-24 15:30:16 -0500

    Auto stash before merge of "master" and "origin/master"
    arreglado

--- routes/router.js
+++ routes/router.js
@@ -20,9 +20,19 @@ router.get('/login', function(req, res){
 router.get('/productos', function(req, res){
 	res.render('productos',{usuario:req.session.username,rango:req.session.rango});
 });
+
+// Carga educativas
 router.get('/educa', function(req, res){
-	res.render('educa',{usuario:req.session.username,rango:req.session.rango});
+	software.findOneC('Educativas',function(error,users){
+		if(error)
+			next(error);
+		else if(!users)
+			users = [];
+		else
+			res.render('educa',{usuario:req.session.username, modelo:users,rango:req.session.rango});
+	});
 });
+
 router.get('/empre', function(req, res){
 	res.render('empre',{usuario:req.session.username,rango:req.session.rango});
 });
@@ -33,17 +43,6 @@ router.get('/servicio', function(req, res){
 	res.render('servicio',{usuario:req.session.username,rango:req.session.rango});
 });
 router.get('/editor', function(req, res){
-	// if(!req.session.username){
-	// 	res.redirect('/');
-	// }
-	// software.findOneSoftware(req.session.username,function(error,users){
-	// 	if(error)
-	// 		next(error);
-	// 	else if(!users)
-	// 		users = [];
-	// 	else
-	// 		res.render('editor',{usuario:req.session.username, modelo:users,rango:req.session.rango});
-	// });
 	res.render('editor',{usuario:req.session.username,rango:req.session.rango});
 });
 router.get('/developers', function(req, res){
@@ -381,7 +380,7 @@ router.get('/search',function(req,res){
 
 router.post('/search_member',function(req,res){
 	console.log(req.body.fullname);
-	software.findOne(req.body.fullname,function(error,users){
+	software.findOneS(req.body.fullname,function(error,users){
 		if(error)
 			next(error);
 		else if(!users)
